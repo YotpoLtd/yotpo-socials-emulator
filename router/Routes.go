1a8cb079557b8572cd1a6c00cc057f179cf124a9
Dmitri Gitelzon <dmitri@yotpo.com>
2015-11-03 14:13:32 +0200

    First commit

---
+++ router/Routes.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"net/http"
+	"yotpo-facebook/handlers"
+)
+
+/*Route structure*/
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+/*Routes array*/
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Authenticate",
+		"GET",
+		"/me/permissions",
+		handlers.Authenticate,
+	},
+	Route{
+		"Authenticate",
+		"GET",
+		"/me",
+		handlers.Authenticate,
+	},
+	Route{
+		"Authenticate",
+		"GET",
+		"/me/accounts",
+		handlers.Authenticate,
+	},
+}
